8fe9ea28ed2ec439c61b1901e3d32b245e9d9a24
Pinae <jonny@pinae.net>
2019-04-18 11:28:51 +0200

    Variant with a "hole" for the head in the logo. This code still works.

--- holePrintPP.py
+++ holePrintPP.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-a, f, p, r = ([109, 80, 110, 97], chr, print, 'r')
-ぴ,な,s=(f(a[1])+"i",        ''.join([f(x) for x in
+a, f, p, r, e = [109, 80, 110, 97], chr, print, 'r', 'utf-8'
+ぴ,な,s=(f(a[1])+"i",        "".join([f(x) for x in
                                      a[2:]]), " ")
 p(s.join([ぴ + な,              f(a[1])+r]),end='')
-rg = str().join([             "OG".lower(), r,
+rg=str().join([             "OG".lower(), r,
                               f(a[-1]), f(a[0])*2])
 p(rg+"ie", end='')
-p(r + chr(bytes(r,             'utf-8')[0]+2))
+p(r +                         f(bytes(r,e)[0]+2))
